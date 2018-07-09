f5686b48a89524202a35d5d9def3f09a2f5c14af
madhead <siarhei.krukau@gmail.com>
2018-07-10 00:36:55 +0300

    Code stub for Lambda

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,33 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+buildscript {
+	dependencies {
+		classpath("de.sebastianboegl.gradle.plugins:shadow-log4j-transformer:2.2.0")
+	}
+}
+
+plugins {
+	kotlin("jvm").version("1.2.51")
+	id("com.github.johnrengelman.shadow").version("2.0.4")
+}
+
+repositories {
+	jcenter()
+}
+
+dependencies {
+	implementation(kotlin("stdlib-jdk8", "1.2.51"))
+	implementation("com.amazonaws:aws-lambda-java-core:1.2.0")
+	implementation("com.amazonaws:aws-lambda-java-events:2.1.0")
+	implementation("com.amazonaws:aws-lambda-java-log4j2:1.1.0")
+	implementation("org.apache.logging.log4j:log4j-api:2.11.0")
+	implementation("org.apache.logging.log4j:log4j-core:2.11.0")
+}
+
+val shadowJar by tasks.getting(ShadowJar::class) {
+	transform(de.sebastianboegl.gradle.plugins.shadow.transformers.Log4j2PluginsFileTransformer::class.java)
+}
+
 task<Wrapper>("wrapper") {
 	gradleVersion = "4.8.1"
 	distributionType = Wrapper.DistributionType.ALL
---
+++ src/main/kotlin/by/dev/madhead/packt4free/Handler.kt
@@ -0,0 +1,16 @@
+package by.dev.madhead.packt4free
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent
+import org.apache.logging.log4j.LogManager
+
+class Handler : RequestHandler<ScheduledEvent, Unit> {
+	companion object {
+		private val logger = LogManager.getLogger(Handler::class.java)
+	}
+
+	override fun handleRequest(input: ScheduledEvent, context: Context) {
+		logger.info("Got request: ${input}")
+	}
+}
