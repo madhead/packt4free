6b9fad527972cc101336fbba6f92b4159663bb31
madhead <siarhei.krukau@gmail.com>
2018-07-11 00:49:41 +0300

    Resolve #2: Packt checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,15 +17,26 @@ repositories {
 
 dependencies {
 	implementation(kotlin("stdlib-jdk8", "1.2.51"))
+
 	implementation("com.amazonaws:aws-lambda-java-core:1.2.0")
 	implementation("com.amazonaws:aws-lambda-java-events:2.1.0")
+
+	implementation("org.jsoup:jsoup:1.11.3")
+
 	implementation("com.amazonaws:aws-lambda-java-log4j2:1.1.0")
 	implementation("org.apache.logging.log4j:log4j-api:2.11.0")
 	implementation("org.apache.logging.log4j:log4j-core:2.11.0")
+
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
+	testRuntime("org.junit.jupiter:junit-jupiter-engine:5.2.0")
 }
 
 val shadowJar by tasks.getting(ShadowJar::class) {
 	transform(de.sebastianboegl.gradle.plugins.shadow.transformers.Log4j2PluginsFileTransformer::class.java)
+}
+
+val test by tasks.getting(Test::class) {
+	useJUnitPlatform()
 }
 
 task<Wrapper>("wrapper") {
