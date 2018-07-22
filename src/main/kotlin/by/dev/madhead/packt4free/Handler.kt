13266e652cf74e9c01bd590016ce4dc19fdff371
madhead <siarhei.krukau@gmail.com>
2018-07-22 13:52:51 +0300

    Resolve #13: Send emails with SES

--- src/main/kotlin/by/dev/madhead/packt4free/Handler.kt
+++ src/main/kotlin/by/dev/madhead/packt4free/Handler.kt
@@ -3,6 +3,8 @@ package by.dev.madhead.packt4free
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder
+import com.amazonaws.services.simpleemail.model.*
 import org.apache.logging.log4j.LogManager
 
 class Handler : RequestHandler<ScheduledEvent, Unit> {
@@ -12,5 +14,20 @@ class Handler : RequestHandler<ScheduledEvent, Unit> {
 
 	override fun handleRequest(input: ScheduledEvent, context: Context) {
 		logger.info("test")
+
+		val simpleEmailService = AmazonSimpleEmailServiceClientBuilder
+			.defaultClient()
+
+		simpleEmailService.sendEmail(
+			SendEmailRequest()
+				.withDestination(Destination().withToAddresses(System.getenv("EMAIL_TO")!!))
+				.withSource(System.getenv("EMAIL_FROM")!!)
+				.withMessage(Message()
+					.withSubject(Content().withCharset("UTF-8").withData("TEST"))
+					.withBody(Body().withText(Content().withCharset("UTF-8").withData("TEST")))
+				)
+		)
+
+		logger.info("email sent")
 	}
 }
